3dec1f25fc282d9e66aab9bdb8c831b248a0e110
Hexa <hexa.diary@gmail.com>
2017-05-21 18:59:56 +0900

    ファイルを分ける

--- src/roost.cr
+++ src/roost.cr
@@ -1,27 +1,5 @@
 require "./roost/*"
 require "option_parser"
-require "http/server"
-require "openssl"
-
-module Roost
-  class Server
-    def self.run(ip_address : String, port : Int, dir = ".", certificates = "", private_key = "")
-      server = HTTP::Server.new(ip_address, port, [
-        HTTP::LogHandler.new,
-        HTTP::StaticFileHandler.new(dir),
-      ])
-
-      unless certificates.empty? || private_key.empty?
-        context = OpenSSL::SSL::Context::Server.new
-        context.certificate_chain = certificates
-        context.private_key = private_key
-        server.tls = context
-      end
-
-      server.listen
-    end
-  end
-end
 
 address = "::"
 port = 8000
