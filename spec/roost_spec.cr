0c0747662f5b517b11578fb3bb8bcb7458f62b1c
Hexa <hexa.diary@gmail.com>
2017-06-23 17:16:05 +0900

    停止できるようにする

--- spec/roost_spec.cr
+++ spec/roost_spec.cr
@@ -8,9 +8,14 @@ describe Roost do
     address = "::"
     port = 8000
 
+    ch = Channel(Roost::Server).new
+
     spawn do
-      Roost::Server.run(address, port)
+      server = Roost::Server.new(address, port)
+      ch.send(server)
+      server.listen
     end
+    server = ch.receive
 
     sleep 1
 
@@ -18,13 +23,15 @@ describe Roost do
     client.get("/") do |response|
       response.status_code.should eq(200)
     end
+
+    server.close
   end
 
   it "" do
     address = "::"
-    port = 9000
-    ws_host = "127.0.0.1"
-    ws_port = 9001
+    port = 8000
+    ws_host = "::1"
+    ws_port = 8001
     ws_path = "/"
     ws_uri = "ws://#{ws_host}:#{ws_port}#{ws_path}"
 
@@ -38,32 +45,37 @@ describe Roost do
       end
     end
 
-    server = HTTP::Server.new(ws_host, ws_port, [ws_handler])
+    ws_server = HTTP::Server.new(ws_host, ws_port, [ws_handler])
     spawn do
-      server.listen
+      ws_server.listen
     end
 
+    ch1 = Channel(Roost::Server).new
     spawn do
-      Roost::Server.run(address, port, ".", "", "", false, true, ws_uri)
+      server = Roost::Server.new(address, port, ".", "", "", false, true, ws_uri)
+      ch1.send(server)
+      server.listen
     end
+    server = ch1.receive
 
     sleep 1
 
-    ch = Channel(String).new
+    ch2 = Channel(String).new
     ws = HTTP::WebSocket.new(ws_host, ws_path, ws_port, false)
     ws.send("test message")
     ws.on_message do |message|
-      ch.send message
+      ch2.send message
     end
 
     spawn do
       ws.run
     end
 
-    message = ch.receive
+    message = ch2.receive
     message.should eq("message")
 
     ws.close
+    ws_server.close
     server.close
   end
 end
--- src/roost/server.cr
+++ src/roost/server.cr
@@ -3,22 +3,33 @@ require "openssl"
 
 module Roost
   class Server
-    def self.run(ip_address : String, port : Int, dir = ".", certificates : String = "", private_key : String = "", verbose : Bool = false, ws : Bool = false, ws_uri : URI | String = "ws://[::1]:8080/")
+    def initialize(ip_address : String, port : Int, dir : String = ".", certificates : String = "", private_key : String = "", verbose : Bool = false, ws : Bool = false, ws_uri : URI | String = "ws://[::1]:8080/")
       handlers = [] of (HTTP::ErrorHandler | HTTP::LogHandler | HTTP::StaticFileHandler | HTTP::WebSocketHandler)
       handlers << HTTP::ErrorHandler.new(verbose)
       handlers << HTTP::LogHandler.new
-      handlers << websocket_handler(ws_uri) if ws
+      handlers << Roost::Server.websocket_handler(ws_uri) if ws
       handlers << HTTP::StaticFileHandler.new(dir)
 
-      server = HTTP::Server.new(ip_address, port, handlers)
+      @server = HTTP::Server.new(ip_address, port, handlers)
 
       unless certificates.empty? || private_key.empty?
         context = OpenSSL::SSL::Context::Server.new
         context.certificate_chain = certificates
         context.private_key = private_key
-        server.tls = context
+        @server.tls = context
       end
+    end
 
+    def listen
+      @server.listen
+    end
+
+    def close
+      @server.close
+    end
+
+    def self.run(ip_address : String, port : Int, dir : String = ".", certificates : String = "", private_key : String = "", verbose : Bool = false, ws : Bool = false, ws_uri : URI | String = "ws://[::1]:8080/")
+      server = self.new(ip_address, port, dir, certificates, private_key, verbose, ws, ws_uri)
       server.listen
     end
 
