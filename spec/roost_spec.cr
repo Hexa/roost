8d19aabf55b4f4d8db3d1df118a12f4b2342ad17
Hexa <hexa.diary@gmail.com>
2017-06-23 16:43:51 +0900

    WebSocket の spec を追加する

--- spec/roost_spec.cr
+++ spec/roost_spec.cr
@@ -1,5 +1,7 @@
 require "./spec_helper"
 require "http/client"
+require "http/server"
+require "http/web_socket"
 
 describe Roost do
   it "" do
@@ -16,5 +18,52 @@ describe Roost do
     client.get("/") do |response|
       response.status_code.should eq(200)
     end
+  end
+
+  it "" do
+    address = "::"
+    port = 9000
+    ws_host = "127.0.0.1"
+    ws_port = 9001
+    ws_path = "/"
+    ws_uri = "ws://#{ws_host}:#{ws_port}#{ws_path}"
+
+    ws_handler = HTTP::WebSocketHandler.new do |context|
+      context.on_message do |message|
+        context.send("message")
+      end
+
+      context.on_close do |message|
+        context.close("close")
+      end
+    end
+
+    server = HTTP::Server.new(ws_host, ws_port, [ws_handler])
+    spawn do
+      server.listen
+    end
+
+    spawn do
+      Roost::Server.run(address, port, ".", "", "", false, true, ws_uri)
+    end
+
+    sleep 1
+
+    ch = Channel(String).new
+    ws = HTTP::WebSocket.new(ws_host, ws_path, ws_port, false)
+    ws.send("test message")
+    ws.on_message do |message|
+      ch.send message
+    end
+
+    spawn do
+      ws.run
+    end
+
+    message = ch.receive
+    message.should eq("message")
+
+    ws.close
+    server.close
   end
 end
