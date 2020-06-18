d2e33f71d8df1d63a1cdc4442cf23fbbbae99357
Hexa <hexa.diary@gmail.com>
2020-06-18 19:58:47 +0900

    コスメ

--- spec/roost_spec.cr
+++ spec/roost_spec.cr
@@ -2,10 +2,11 @@ require "./spec_helper"
 require "http/client"
 require "http/server"
 require "http/web_socket"
+require "uri"
 
 describe Roost do
   it "" do
-    ip_address = "::"
+    ip_address = "localhost"
     port = 8000
 
     ch = Channel(Roost::Server).new
@@ -19,7 +20,7 @@ describe Roost do
 
     sleep 1
 
-    client = HTTP::Client.new("::1", port)
+    client = HTTP::Client.new("localhost", port)
     client.get("/") do |response|
       response.status_code.should eq(200)
     end
@@ -28,12 +29,9 @@ describe Roost do
   end
 
   it "" do
-    ip_address = "::"
+    ip_address = "localhost"
     port = 8000
-    ws_host = "::1"
-    ws_port = 8001
-    ws_path = "/"
-    ws_uri = "ws://#{ws_host}:#{ws_port}#{ws_path}"
+    ws_uri = URI.new(scheme = "ws", host = "localhost", port = 8001, path = "/")
 
     ws_handler = HTTP::WebSocketHandler.new do |ws, context|
       ws.on_message do |message|
@@ -45,17 +43,17 @@ describe Roost do
       end
     end
 
-    TestWSServer.run(ws_host, ws_port, [ws_handler]) do
+    TestWSServer.run(ws_uri.host || "localhost", ws_uri.port || 8001, [ws_handler]) do
       ch = Channel(Roost::Server).new
       spawn do
-        server = Roost::Server.new(ip_address: ip_address, port: port, public_dir: ".", ws_uri: ws_uri)
+        server = Roost::Server.new(ip_address: ip_address, port: port, public_dir: ".", ws_uri: ws_uri.to_s)
         ch.send(server)
         server.listen
       end
       server = ch.receive
 
       sleep 1
-      response_message = TestWSClient.send_receive(ws_host, ws_path, ws_port, "test message")
+      response_message = TestWSClient.send_receive(ws_uri, "test message")
       response_message.should eq("message")
       server.close
     end
